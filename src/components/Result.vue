cb27230859b1d12a51a84112a4eca35b11372daf
HoCG <rhwkd5012@naver.com>
2021-09-20 18:48:13 +0900

    위치 수정

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,11 +1,26 @@
 <template>
-    <v-layout row="row" wrap="wrap" width="100%" justify-center="justify-center">
-        <v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
-            max-height="300"
-            max-width="600"
-            src="https://picsum.photos/id/11/500/300"></v-img>
-    </v-layout>
+    <div class="SecondApplication">
+        <v-layout row="row" wrap="wrap" width="100%" justify-center="justify-center">
+            <v-card-actions class="justify-center" max-width="70%">
+                <v-card>
+                    <v-list>
+                        <v-img
+                            lazy-src="https://picsum.photos/id/11/10/6"
+                            max-height="300"
+                            max-width="600"
+                            src="https://picsum.photos/id/11/500/300"></v-img>
+                    </v-list>
+                </v-card>
+            </v-card-actions>
+        </v-layout>
+    </div>
 </template>
 <script></script>
-<style></style>
+<style>
+    .SecondApplication {
+        display: block;
+        text-align: center;
+        margin-top: 5%;
+        margin-bottom: 5%;
+    }
+</style>

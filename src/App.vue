74afbd2c59a66eb9c705e1bb10719f6d19ef5c60
HoCG <rhwkd5012@naver.com>
2021-09-20 18:48:02 +0900

    설정 수정

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-    <v-app class="justify-center" src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
-        <router-view/>
+    <v-app class="justify-center">
+        <v-img
+            width="100%"
+            height="100%"
+            src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+            <router-view/></v-img>
     </v-app>
 </template>
 
 <script>
     export default {
         name: 'App',
-        components: {
-        },
+        components: {},
         data: () => ({
             //
         })
